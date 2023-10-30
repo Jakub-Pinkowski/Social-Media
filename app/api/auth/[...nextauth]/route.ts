782f6a75447df63d205fa47d3d1dfd343e8b2b0b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-30 20:03:04 +0100

    Working on new provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import GithubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '@/lib/prisma'
+import EmailProvider from 'next-auth/providers/email'
 
 export const authOptions: NextAuthOptions = {
     session: {
@@ -15,6 +16,17 @@ export const authOptions: NextAuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST!,
+                port: Number(process.env.EMAIL_SERVER_PORT!),
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER!,
+                    pass: process.env.EMAIL_SERVER_PASSWORD!,
+                },
+            },
+            from: process.env.EMAIL_FROM!,
         }),
         CredentialsProvider({
             name: 'as Guest',
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 export default async function Home() {
     const session = await getServerSession()
 
+    console.log('index page line 7 ' + session?.user?.email)
     if (!session) {
         redirect('/api/auth/signin')
     }
